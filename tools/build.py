e5d5b534ad5fe8fe8679c612c3f20670d67cf4c9
Santhosh Thottingal <santhosh.thottingal@gmail.com>
2015-09-27 16:59:09 +0530

    Unlink the references before build

--- tools/build.py
+++ tools/build.py
@@ -4,17 +4,20 @@ import fontforge
 font = fontforge.open(sys.argv[1])
 # Remove all GSUB lookups
 for lookup in font.gsub_lookups:
-	font.removeLookup(lookup)
+    font.removeLookup(lookup)
 
-# Remove all GPOS lookups 
+# Remove all GPOS lookups
 for lookup in font.gpos_lookups:
-	font.removeLookup(lookup)        
+    font.removeLookup(lookup)
 
-# Merge the new featurefile 
+# Merge the new featurefile
 font.mergeFeature(sys.argv[2])
-font.appendSFNTName('English (US)', 'Version', sys.argv[3] + '+' + time.strftime('%Y%m%d'))
+font.appendSFNTName('English (US)', 'Version', sys.argv[3]
+    + '+' + time.strftime('%Y%m%d'))
 font.selection.all()
+font.addExtrema()
+font.unlinkReferences()
+font.removeOverlap()
 font.simplify()
-font.round()
-font.generate(sys.argv[1].replace(".sfd",".otf"), flags=("omit-instructions", "round", "opentype"))
-font.close() 
+font.generate(sys.argv[1].replace(".sfd", ".otf"), flags=("round", "opentype"))
+font.close()
