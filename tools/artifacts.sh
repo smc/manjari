b39e2ed9c60fef9919e817bbb04f9c87b097cfe5
Santhosh Thottingal <santhosh.thottingal@gmail.com>
2020-09-01 18:16:03 +0530

    CI: Add LICENSE.txt and README.md to artifacts
    Fixes issue#27

--- tools/artifacts.sh
+++ tools/artifacts.sh
@@ -1,4 +1,4 @@
-cp *.conf build
+cp *.conf LICENSE.txt README.md build
 
 cd build
 rm -rf ttf otf woff woff2 *.pdf
@@ -9,10 +9,10 @@ cp *.otf otf
 cp *.woff woff
 cp *.woff2 woff2
 
-tar czvf ${CI_PROJECT_NAME}-${CI_COMMIT_TAG}.tar.gz *.conf ttf otf
+tar czvf ${CI_PROJECT_NAME}-${CI_COMMIT_TAG}.tar.gz *.conf LICENSE.txt README.md ttf otf
 sha256sum ${CI_PROJECT_NAME}-${CI_COMMIT_TAG}.tar.gz > ${CI_PROJECT_NAME}-${CI_COMMIT_TAG}.tar.gz.sha256
 md5sum ${CI_PROJECT_NAME}-${CI_COMMIT_TAG}.tar.gz > ${CI_PROJECT_NAME}-${CI_COMMIT_TAG}.tar.gz.md5
-zip -j ${CI_PROJECT_NAME}-${CI_COMMIT_TAG}.zip otf/*
+zip -j ${CI_PROJECT_NAME}-${CI_COMMIT_TAG}.zip README.md LICENSE.txt otf/*
 
 ln -s ${CI_PROJECT_NAME}-${CI_COMMIT_TAG}.tar.gz ./${CI_PROJECT_NAME}_LATEST.tar.gz
 ln -s ${CI_PROJECT_NAME}-${CI_COMMIT_TAG}.tar.gz.sha256 ./${CI_PROJECT_NAME}_LATEST.tar.gz.sha256
